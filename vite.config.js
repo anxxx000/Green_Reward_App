dbcbea32d42b3a095a4b4162b3c5a7fa2aed1460
anxxx000 <an.n.xu@mail.mcgill.ca>
2025-10-26 17:41:37 -0400

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Green_Reward_App",
+  base: "/Green_Reward_App",
 });
